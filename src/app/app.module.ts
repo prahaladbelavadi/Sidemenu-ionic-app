857b4a23f589d56ec79386e0a7a0245352f2cc31
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2018-08-12 11:56:33 -0700

    add index

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
 import { Page1Page } from '../pages/page1/page1';
 import { Page2Page } from '../pages/page2/page2';
+import { Page3Page } from '../pages/page3/page3';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -17,7 +18,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     HomePage,
     ListPage,
     Page1Page,
-    Page2Page
+    Page2Page,
+    Page3Page
   ],
   imports: [
     BrowserModule,
@@ -29,7 +31,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     HomePage,
     ListPage,
     Page1Page,
-    Page2Page
+    Page2Page,
+    Page3Page
   ],
   providers: [
     StatusBar,
