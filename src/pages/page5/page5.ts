1b880a86d9587a2fc3ea7212dd7c65de1e941ede
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2018-08-14 19:27:46 +0530

    try different example

--- src/pages/page5/page5.ts
+++ src/pages/page5/page5.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ItemSlidiing, ToastController } from 'ionic-angular';
 
 /**
  * Generated class for the Page5Page page.
@@ -14,8 +14,84 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'page5.html',
 })
 export class Page5Page {
+  chats: any[];
+  logins: any[];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public toastCtrl: ToastController) {
+
+    this.chats = [
+      {
+        img: './assets/avatar-cher.png',
+        name: 'Cher',
+        message: 'Ugh. As if.',
+        time: '9:38 pm'
+      }, {
+        img: './assets/avatar-dionne.png',
+        name: 'Dionne',
+        message: 'Mr. Hall was way harsh.',
+        time: '8:59 pm'
+      }, {
+        img: './assets/avatar-murray.png',
+        name: 'Murray',
+        message: 'Excuse me, "Ms. Dione."',
+        time: 'Wed'
+      }
+    ];
+    this.logins = [
+      {
+          icon: 'logo-twitter',
+          name: 'Twitter',
+          username: 'admin',
+      }, {
+          icon: 'logo-github',
+          name: 'GitHub',
+          username: 'admin37',
+      }, {
+          icon: 'logo-instagram',
+          name: 'Instagram',
+          username: 'imanadmin',
+      }, {
+          icon: 'logo-codepen',
+          name: 'Codepen',
+          username: 'administrator',
+      }];
+  }
+
+  more(item: ItemSliding){
+    console.log('More');
+    item.close()
+  }
+
+  delete(item: ItemSliding){
+    console.log('Delete')
+    item.close()
+  }
+
+  mute(item: ItemSliding){
+    console.log('mute')
+    item.close()
+  }
+
+  archive(item: ItemSliding){
+    this.expandAction(item, 'archivng','Chat was archived')
+  }
+
+  download(item: ItemSliding){
+    this.expandAction(item, 'downloading', 'Login was downloaded');
+  }
+
+  expandAction(item: ItemSliding, _: any, text: string){
+    // do actions
+    setTimeout(()=>{
+    const toast = this.toastCtrl.create({
+      message: text
+    });
+    toast.present();
+    item.close()
+
+    setTimeout(()=> toast.dismiss(), 2000);
+  }, 1500);
   }
 
   ionViewDidLoad() {
