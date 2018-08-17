7870b02d8692f760fc5764ccbac6d1e4ca5e5376
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2018-08-18 03:06:20 +0530

    setup toasts page

--- src/pages/page6/page6.ts
+++ src/pages/page6/page6.ts
@@ -18,13 +18,39 @@ export class Page6Page {
   constructor(public navCtrl: NavController, public navParams: NavParams, public toastCtrl: ToastController) {}
     topToast() {
       const toast = this.toastCtrl.create({
-        message: 'Button was clicked',
-        duration: 300
+        message: 'Top toast Button was clicked',
+        duration: 3000,
+        position: 'top'
       });
       toast.present()
     }
 
+    bottomToast(){
+      const toast = this.toastCtrl.create({
+        message: 'Bottom toast Button was clicked',
+        duration: 3000,
+        position: 'bottom'
+      });
+      toast.present()
+    }
 
+    longToast(){
+      const toast = this.toastCtrl.create({
+        message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        duration: 1500,
+        position: 'bottom'
+      });
+      toast.present()
+    }
+
+    showToast(position:string){
+      const toast = this.toastCtrl.create({
+        message: 'Button was clicked',
+        duration: 500,
+        position: position
+      });
+      toast.present()
+    }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad Page6Page');
