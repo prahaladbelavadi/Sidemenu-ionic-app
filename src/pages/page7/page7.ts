302bf222a33d17a4bfa1ccc0e1bb490f8c1ac703
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2018-08-18 17:28:42 +0530

    search functionality

--- src/pages/page7/page7.ts
+++ src/pages/page7/page7.ts
@@ -14,7 +14,26 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'page7.html',
 })
 export class Page7Page {
+  items: Array<String>;
 
+  ngOnInit(){
+    this.setItems();
+  }
+
+  setItems(){
+    this.items = ['Orange', 'Banana', 'Pear', 'Tomato', 'Grape', 'Apple', 'Cherries', 'Cranberries', 'Raspberries', 'Strawberries', 'Watermelon'];
+  }
+
+  filterItems(ev: any){
+    this.setItems();
+    let val = ev.target.value;
+
+    if (val && val.trim() !== ''){
+      this.items = this.items.filter(function(item){
+        return item.toLowerCase().includes(val.toLowerCase());
+      });
+    }
+  }
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
